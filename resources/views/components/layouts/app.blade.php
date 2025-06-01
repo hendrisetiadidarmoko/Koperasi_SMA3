cfaab947f600da6ba9b96c3ed1fb80eb19da9399
hendrisetiadidarmoko <21102187@ittelkom-pwt.ac.id>
2025-06-02 01:11:44 +0700

    Last v3

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,7 +11,10 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         {{-- <script src="https://unpkg.com/html5-qrcode" type="text/javascript"> --}}
 
-
+        {{-- font --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     
